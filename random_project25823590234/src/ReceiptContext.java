14db915dae5471d7bc785a3f073112cf64fefe9c
nicolas <nicolas@10.250.2.78>
2016-12-07 10:11:28 -0800

    Added some changes and did work on Receipt strategy and Receipt Context.

--- random_project25823590234/src/ReceiptContext.java
+++ random_project25823590234/src/ReceiptContext.java
@@ -7,7 +7,12 @@ public class ReceiptContext
 	{
 		this.receipt = receipt;
 	}
-	
+	/**
+	 * Executes the strategy of either simple or comprehensive Receipt
+	 * @param id
+	 * @param name
+	 * @param account
+	 */
 	public void executeStrategy(int id, String name, Account account)
 	{
 		receipt.showReceipt(id, name, account);
