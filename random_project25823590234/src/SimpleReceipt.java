89fae04f78eede63ad41d115808755747f5c7412
nicolas <nicolas@10.250.2.78>
2016-12-07 10:08:18 -0800

    Added some changes and did work on Receipt Stratagy and Receipt Context.

--- random_project25823590234/src/SimpleReceipt.java
+++ random_project25823590234/src/SimpleReceipt.java
@@ -1,4 +1,12 @@
 
-public class SimpleReceipt implements Receipt {
-
+public class SimpleReceipt implements Receipt 
+{
+	public void showReceipt(int id, String name, Account account)
+	{
+		if (account.getName() == name && account.getID() == id)
+		{
+			System.out.print("User ID: " + id + ", Name: " + name);
+			// TO DO implement method to return all reservered rooms (NOT RESERVATIONS) by User and corresponding amount due
+		}
+	}
 }
