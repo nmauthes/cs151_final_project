7feaa251ada33a829177b1babbde86332abc8d7d
Zahra <zahraamin48@gmail.com>
2016-12-07 20:50:29 -0800

    Added methods to RS & GV, added InitialView class

---
+++ random_project25823590234/src/InitialView.java
@@ -0,0 +1,16 @@
+
+
+
+public class InitialView {
+	
+	/*
+	 * TODO
+	 * buttons needed: "Guest" button, "Manager" button
+	 * "Guest" button pressed --> calls Model's guest()
+	 * "Manager" button pressed --> calls Model's manager()
+	 */
+	public void mainMenuDisplay() {
+		
+	}
+
+}
